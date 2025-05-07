e6275740e2a920c43a92ce876d7ee4e5cf016d05
KopralProject <kopralwann04@gmail.com>
2025-05-07 11:20:17 +0700

    Update kemaskini.sh

--- update/kemaskini.sh
+++ update/kemaskini.sh
@@ -11,10 +11,10 @@ sleep 1
 #rm -f /usr/bin/usernew
 #rm -f /usr/bin/menu-vless
 #download
-#wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/V3SAKURAAIRIV3/BotDejavuV3/main/menu/menu.sh" && chmod +x /usr/bin/menu
-#wget -q -O /usr/bin/setting "https://raw.githubusercontent.com/V3SAKURAAIRIV3/BotDejavuV3/main/menu/setting.sh"
-#wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/V3SAKURAAIRIV3/BotDejavuV3/main/usernew.sh"
-#wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/V3SAKURAAIRIV3/BotDejavuV3/main/menu-vless.sh"
+#wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/KopralProject/BotDejavuV3/main/menu/menu.sh" && chmod +x /usr/bin/menu
+#wget -q -O /usr/bin/setting "https://raw.githubusercontent.com/KopralProject/BotDejavuV3/main/menu/setting.sh"
+#wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/KopralProject/BotDejavuV3/main/usernew.sh"
+#wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/KopralProject/BotDejavuV3/main/menu-vless.sh"
 #echo "*/3 * * * * root bot" >> /etc/crontab
 #izin
 #chmod 777 /usr/bin/usernew
